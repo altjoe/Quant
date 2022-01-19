bb6f73df95b347b55f033acf6b30d54984e8504e
altjoe <meyer.joer5@gmail.com>
2022-01-19 09:39:29 -0500

    working throughh excersize 2.8.1

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 import Quant as quant
 import scipy.stats as st
-
+import plotly.express as px
 
 REMOTE_HOST = 'mi3-ss64.a2hosting.com'
 REMOTE_SSH_PORT = 7822
@@ -14,22 +14,34 @@ SSH_USERNAME = 'altjoeah'
 SSH_PASSWORD = 'f3N03rll:)'
 REMOTE_BIND_ADDR = ('localhost', 5432)
 LOCAL_BIND_ADDR = REMOTE_BIND_ADDR
-
+pd.set_option('display.max_columns', None)
 def main():
-    df = pd.read_csv('RickySteve.csv')
-
-    ricky = list(df['Ricky'].to_numpy(dtype='float64'))
-    steve = df['Steve'].to_numpy(dtype='float64')
+    df = pd.read_csv('2-8-1-Excersize.csv')
+    df['FORDRET'] = quant.simple_returns(df['FORD'].values)
+    df['GERET'] = quant.simple_returns(df['GE'].values)
+    df['MSOFTRET'] = quant.simple_returns(df['MICROSOFT'].values)
     
-    print(quant.covariance(ricky, steve))
-    print(quant.correlation(ricky, steve))
+    return_columns = ['FORDRET', 'GERET', 'MSOFTRET']
+    weights = np.asanyarray([0.33, 0.33, 0.34])
+    average_returns = np.asanyarray(df[return_columns].mean())
+    df['PORTRET'] = quant.portfolio_returns(df[return_columns], weights)
 
-    # stat_df = pd.DataFrame()
-    # stat_df = stat_df.append(simple_stats(ricky, 'ricky'))
-    # stat_df = stat_df.append(simple_stats(steve, 'steve'))
-    # print(stat_df) 
+    
+    covariance = quant.covariance_matrix(df[return_columns])
+    print(covariance)
+    print(f'Average Returns: {average_returns}')
 
+    def portfolio_mean(weights, returns):
+        return np.sum(np.asanyarray(returns.mean()) * weights)
+
+    print(portfolio_mean(weights, df[return_columns]))
+    variance = np.dot(np.dot(np.transpose(weights), covariance), weights)
+    print(variance)
+    #pg 137
+    # print(df)
+
+    
 
 # with SSHTunnelForwarder((REMOTE_HOST, REMOTE_SSH_PORT), ssh_username=SSH_USERNAME, ssh_password=SSH_PASSWORD,
 #                             remote_bind_address=REMOTE_BIND_ADDR, local_bind_address=LOCAL_BIND_ADDR):
-main()
+main() 
